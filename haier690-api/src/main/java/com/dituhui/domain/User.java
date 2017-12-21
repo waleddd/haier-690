52accc715fdaa4a0562f71ceb977d9ad176eac30
kuayue <1358236712@qq.com>
2017-12-21 10:30:40 +0800

    初始化

--- haier690-api/src/main/java/com/dituhui/domain/User.java
+++ haier690-api/src/main/java/com/dituhui/domain/User.java
@@ -11,4 +11,45 @@ public class User implements Serializable {
     private String password;
     private Integer age;
     private Integer gender;
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public void setGender(Integer gender) {
+        this.gender = gender;
+    }
+
+    public Integer getId() {
+
+        return id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public Integer getGender() {
+        return gender;
+    }
 }
